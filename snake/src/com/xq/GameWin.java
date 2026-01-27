86df1e4c6765f22764a3f4188735696af11e5ff0
XQ1997 <2645564458@qq.com>
2026-01-27 23:26:42 +0800

    定义游戏物体的父类

--- snake/src/com/xq/GameWin.java
+++ snake/src/com/xq/GameWin.java
@@ -1,3 +1,5 @@
+package com.xq;
+
 import javax.swing.*;
 import java.awt.*;
 
---
+++ snake/src/com/xq/obj/GameObj.java
@@ -0,0 +1,84 @@
+package com.xq.obj;
+
+import com.xq.GameWin;
+import java.awt.*;
+/**定义游戏物体的父类
+ * @author 26455
+ */
+public class GameObj {
+    //图片
+    Image img;
+    //坐标
+    int x;
+    int y;
+    //宽高
+    int width = 30;
+    int height = 30;
+    //窗口类的引用
+    GameWin frame;
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public GameWin getFrame() {
+        return frame;
+    }
+
+    public void setFrame(GameWin frame) {
+        this.frame = frame;
+    }
+
+    public GameObj() {
+    }
+
+    public GameObj(Image img, int x, int y, int width, int height, GameWin frame) {
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.frame = frame;
+    }
+
+    //绘制自身
+    public void paintSelf(Graphics g){
+        g.drawImage(img,x,y,null);
+    }
+}
