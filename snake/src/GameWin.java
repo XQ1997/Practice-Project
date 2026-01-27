1d94637f3ff1e0174b07aac07e22a5d8e81877b5
XQ1997 <2645564458@qq.com>
2026-01-27 23:16:31 +0800

    绘制网格

--- snake/src/GameWin.java
+++ snake/src/GameWin.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**贪吃蛇小游戏
  * @author 26455
@@ -14,6 +15,25 @@ public class GameWin extends JFrame {
         this.setLocationRelativeTo(null);
         //设置窗口的标题
         this.setTitle("贪吃蛇小游戏");
+    }
+
+    //重写pint方法
+    @Override
+    public void paint(Graphics g){
+        //灰色背景
+        g.setColor(Color.gray);
+        //填充矩形
+        g.fillRect(0,0,600,600);
+        //绘制网格线
+        g.setColor(Color.black);
+        //网格的本质是多条线相交组成
+        //为了批量绘制直线使用for循环
+        for(int i= 0;i<=20;i++){
+            // 先绘制横线 前两个坐标是起始坐标，后两个是终点坐标
+            g.drawLine(0,i * 30,600,i * 30);
+            //竖线
+            g.drawLine(i * 30,0,i * 30,600);
+        }
     }
 
     public static void main(String[] args) {
