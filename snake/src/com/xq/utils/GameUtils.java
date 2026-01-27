88b76efb6fad3fc8ed75e72cbe331466c5e91c8b
XQ1997 <2645564458@qq.com>
2026-01-27 23:47:51 +0800

    创建游戏工具类

---
+++ snake/src/com/xq/utils/GameUtils.java
@@ -0,0 +1,26 @@
+package com.xq.utils;
+
+import java.awt.*;
+
+/**创建游戏工具类
+ * @author 26455
+ */
+public class GameUtils {
+
+    //蛇头
+    public static Image upImg = Toolkit.getDefaultToolkit().getImage("img/up.png");
+    public static Image downImg = Toolkit.getDefaultToolkit().getImage("img/down.png");
+    public static Image leftImg = Toolkit.getDefaultToolkit().getImage("img/left.png");
+    public static Image rightImg = Toolkit.getDefaultToolkit().getImage("img/right.png");
+    //蛇身
+    public static Image bodyImg = Toolkit.getDefaultToolkit().getImage("img/body.png");
+    //食物
+    public static Image foodImg = Toolkit.getDefaultToolkit().getImage("img/food.png");
+
+    //绘制文字
+    public static void drawWord(Graphics g,String str,Color color,int size,int x,int y){
+        g.setColor(color);
+        g.setFont(new Font("仿宋",Font.BOLD,size));
+        g.drawString(str,x,y);
+    }
+}
